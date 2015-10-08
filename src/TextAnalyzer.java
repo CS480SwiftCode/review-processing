17427a1b020820f1336b70d7be78ff42c8c04b4a
jon-bassi <jonbassi10@gmail.com>
2015-10-08 13:33:24 -0700

    first version of review analyzer needs testing on more data

--- src/TextAnalyzer.java
+++ src/TextAnalyzer.java
@@ -1,5 +1,75 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Scanner;
+
 
 public class TextAnalyzer
 {
+   public static ArrayList<String> analyze(ArrayList<String> reviews) throws FileNotFoundException
+   {
+      
+      reviews = removeStopwords(reviews);
+      
+      ArrayList<String> data = new ArrayList<>();
+      for (String s : reviews)
+      {
+         String[] sentences = s.split("[.!?]");
+         
+         for (String sn : sentences)
+         {
+            if (sn.contains("happy hour"))
+            {
+               String time = findTime(sn);
+               if (!time.equals(""))
+                  data.add(time);
+            }
+         }
+      }
+      return data;
+   }
    
+   
+   public static ArrayList<String> removeStopwords(ArrayList<String> reviews) throws FileNotFoundException
+   {
+      // should probably make an object which does this to keep words in memory
+      Scanner file = new Scanner(new File("stopwords/stopwords-basic"));
+      HashSet<String> wordlist = new HashSet<>();
+      while (file.hasNext())
+      {
+         wordlist.add(file.nextLine());
+      }
+      file.close();
+      
+      for (int i = 0; i < reviews.size(); i++)
+      {
+         String[] words = reviews.get(i).split(" ");
+         String newSentence = "";
+         for (String s : words)
+         {
+            if (!wordlist.contains(s))
+               newSentence += s + " ";
+         }
+         
+         reviews.set(i, newSentence);
+      }
+      
+      return reviews;
+   }
+   
+   
+   public static String findTime(String sentence)
+   {
+      
+      if (sentence.matches("(?s).*\\d.*"))
+      {
+         int idx = sentence.indexOf("happy hour");
+         sentence = sentence.substring(idx+10,sentence.length());
+         
+         
+         return sentence;
+      }
+      return "";
+   }
 }
--- src/WebScraper.java
+++ src/WebScraper.java
@@ -1,5 +1,39 @@
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
 public class WebScraper
 {
+   // testing this class
+   public static void main(String[] args) throws IOException
+   {
+      // url to yelp page or at least info from yelp on the project
+      String url = "insert url here";
+      
+      ArrayList<String> reviews = retrieveReviews(url);
+      
+      ArrayList<String> data = TextAnalyzer.analyze(reviews);
+      
+      System.out.println(data);
+   }
    
+   
+   private static ArrayList<String> retrieveReviews(String url) throws IOException
+   {
+      Document doc = Jsoup.connect(url).ignoreContentType(true).get();
+      
+      String[] reviews = doc.select("p[itemprop]").toString().split("\n");
+      ArrayList<String> formattedReviews = new ArrayList<>();
+      for (String s : reviews)
+      {
+         s = s.substring(36,s.length()-4);
+         s = s.replace("<br />", "");
+         s = s.toLowerCase();
+         formattedReviews.add(s);
+      }
+      
+      return formattedReviews;
+   }
 }
